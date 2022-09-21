50d14bdf09c058a4fcd3abaa1bb60a4c1e396cbf
Y. R. F. Wang <44557467+Sifyrena@users.noreply.github.com>
2022-09-21 15:29:38 +1200

    Update Current.py

--- PyUltraLight2/Current.py
+++ PyUltraLight2/Current.py
@@ -2861,7 +2861,7 @@ def Load_Data(save_path,ts,save_options,save_number):
             
             EndNum += 1
         
-        except FileNotFoundError:
+        except:
 
             print("WARNING: Run incomplete or the storage is corrupt!")
 
